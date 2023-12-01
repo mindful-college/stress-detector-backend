db66f70171ed245ad6b4ca28debb4c29dbfc5f05
Jiji14 <jijeong0114@gmail.com>
2023-11-30 23:00:00 -0800

    Delete Dummy Data and connect to DB

--- routers/users.py
+++ routers/users.py
@@ -10,6 +10,8 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
 from fastapi import APIRouter
+from schemas import user
+from database import db
 
 load_dotenv()
 router = APIRouter()
@@ -18,36 +20,18 @@ ALGORITHM = os.getenv("ALGORITHM")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ACCESS_TOKEN_EXPIRE_DAYS = int(os.getenv("ACCESS_TOKEN_EXPIRE_DAYS"))
 
-fake_users_db = {
-    "jiji@example.com": {
-        "email" : "jiji@example.com",
-        "name": "jiji",
-        "uuid": "uuid_jiji",
-        "password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW", #secret
-        "disabled": False,
-    }
-}
-
 class Token(BaseModel):
     access_token: str
     token_type: str
-
-class TokenData(BaseModel):
-    email: str
 
 class User(BaseModel):
     email: str
     name: str
 
-class UserInDB(User):
-    password: str
-    disabled: bool
-    uuid: str
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
-
+user_collection = db.get_collection("user")
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
@@ -57,20 +41,14 @@ def get_password_hash(password) -> str:
     return pwd_context.hash(password)
 
 
-def get_user(db, email: str) -> UserInDB:
-    if email in db:
-        user_dict = db[email]
-        return user_dict
-
-
-def authenticate_user(fake_db, email: str, password: str) -> UserInDB:
-    user = get_user(fake_db, email)
+def authenticate_user(email: str, password: str) -> user.UserSchema | None:
+    user = user_collection.find_one({"email" : email})
 
     if not user:
-        return False
+        return None
 
     if not verify_password(password, user["password"]):
-        return False
+        return None
 
     return user
 
@@ -83,35 +61,33 @@ def create_access_token(data: dict, expires_delta: timedelta) -> str:
     return encoded_jwt
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User:
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    print(token)
+
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email: str = payload.get("sub")
-        print(email)
         if email is None:
             raise credentials_exception
-        token_data = TokenData(email=email)
+
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, email=token_data.email)
-    print("user")
-    print(user)
+    user = user_collection.find_one({"email" : email})
+
     if user is None or user["disabled"] is True:
         raise credentials_exception
     return user
 
-
+@router.post("/token", response_model=Token, tags=["users"])
 @router.post("/signin", response_model=Token, tags=["users"])
 def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ):
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    user = authenticate_user(form_data.username, form_data.password)
 
     if not user:
         raise HTTPException(
@@ -129,7 +105,7 @@ def login_for_access_token(
 
 
 @router.get("/users/me", response_model=User, tags=["users"])
-async def read_users_me(
+def read_users_me(
     current_user: Annotated[User, Depends(get_current_user)]
 ):
     return {
