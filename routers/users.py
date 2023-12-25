b57df760fc52164523f11b00eb580d27ab28102e
Pangwoo <gwangwoo25@gmail.com>
2023-12-25 00:52:24 -0800

    update account api

--- routers/users.py
+++ routers/users.py
@@ -190,3 +190,29 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     if user is None or user["disabled"] is True:
         raise credentials_exception
     return {"name": user['name'], "points":user['points']}
+
+
+@router.post("/v1/userinfo", tags=["users"])
+async def update_current_user(token: Annotated[str, Depends(oauth2_scheme)], new_name: str):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        # token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    
+    if new_name is None:
+        raise credentials_exception
+
+    result = user_collection.update_one({"email" : email},{"$set":{"name":new_name}})
+   
+   
+    return {"new_name":new_name}
+
